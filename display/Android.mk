3d30116380a60c0619de46dc8d72684ea8f8fa7b
SGCMarkus <markusornik@gmail.com>
2018-02-25 19:37:43 +0100

    added display
    Change-Id: Ia69e9bef0478bdf73e582031a2d894cd4884f1e3

---
+++ display/Android.mk
@@ -0,0 +1,8 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := qdcm_calib_data_SW43402_cmd_mode_dsc_dsi_panel.xml
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := qdcm_calib_data_SW43402_cmd_mode_dsc_dsi_panel.xml
+include $(BUILD_PREBUILT)
